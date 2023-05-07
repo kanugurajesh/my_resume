f19b09adf36ab1a72fdd6ce141571ad82e463ce8
Rajesh <245122733172@mvsrec.edu.in>
2023-05-07 10:50:13 -0400

    adding the data

---
+++ index.js
@@ -0,0 +1,9 @@
+let password = "explorer"
+
+let pass = prompt("enter the password")
+
+if (pass == password) {
+    alert("welcome to my resume")
+} else {
+    window.location.href = "https://www.google.com"
+}
