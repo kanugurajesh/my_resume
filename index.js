728d9d73b25abd178fdc75e5d15a5cbab344eca2
Rajesh <245122733172@mvsrec.edu.in>
2023-05-07 11:29:02 -0400

    adding the files

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
-// let password = "explorer"
+let box = document.getElementById("box")
+let border = document.getElementById("border")
 
-// let pass = prompt("enter the password")
+let colors = ["red", "green", "blue", "yellow", "orange", "pink", "purple", "brown", "grey"]
 
-// if (pass == password) {
-//     alert("welcome to my resume")
-// } else {
-//     window.location.href = "https://www.google.com"
-// }
+function changeColor() {
+    let random = Math.floor(Math.random() * colors.length)
+    box.style.borderColor = colors[random]
+}
+
+let first = document.getElementById("margin0")
+let second = document.getElementById("margin1")
+let third = document.getElementById("margin2")
+
+// randomly set the linear gradient of the id "margin0", "margin1", "margin2"
+
+function changeBackground() {
+    let random1 = Math.floor(Math.random() * colors.length)
+    let random2 = Math.floor(Math.random() * colors.length)
+    first.style.background = "linear-gradient(to right, " + colors[random1] + ", " + colors[random2] + ")"
+    random1 = Math.floor(Math.random() * colors.length)
+    random2 = Math.floor(Math.random() * colors.length)
+    second.style.background = "linear-gradient(to right, " + colors[random1] + ", " + colors[random2] + ")"
+    random1 = Math.floor(Math.random() * colors.length)
+    random2 = Math.floor(Math.random() * colors.length)
+    third.style.background = "linear-gradient(to right, " + colors[random1] + ", " + colors[random2] + ")"
+}
+
+function changeFontColor() {
+    let random = Math.floor(Math.random() * colors.length)
+    first.style.color = colors[random]
+    random = Math.floor(Math.random() * colors.length)
+    second.style.color = colors[random]
+    random = Math.floor(Math.random() * colors.length)
+    third.style.color = colors[random]
+}
+// set random border radius for the id "border"
+
+function changeBorderRadius() {
+    let random = Math.floor(Math.random() * 10)
+    border.style.borderRadius = random + "%"
+}
+
+// can you call all the function using a single setInterval function
+
+function changeAll() {
+    changeColor()
+    changeBackground()
+    changeFontColor()
+    changeBorderRadius()
+}
+
+setInterval(changeAll, 1000)
