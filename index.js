7290dce0698ad435b2c70c9d673b2f44c62de824
Rajesh <245122733172@mvsrec.edu.in>
2023-05-07 11:08:09 -0400

    adding the files

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let password = "explorer"
+// let password = "explorer"
 
-let pass = prompt("enter the password")
+// let pass = prompt("enter the password")
 
-if (pass == password) {
-    alert("welcome to my resume")
-} else {
-    window.location.href = "https://www.google.com"
-}
+// if (pass == password) {
+//     alert("welcome to my resume")
+// } else {
+//     window.location.href = "https://www.google.com"
+// }
